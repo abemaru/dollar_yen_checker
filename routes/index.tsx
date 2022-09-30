5d2b8c790928ae20afc9b2439144238e6a2880bc
abemaru <ch4nge.lightening@gmail.com>
2022-10-01 01:44:10 +0900

    簡単なアプリ

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,18 +1,54 @@
-import Counter from "../islands/Counter.tsx";
+import { tw } from "twind";
+import { h } from "preact";
+import { Handlers, PageProps } from "$fresh/server.ts"
 
-export default function Home() {
+const url:string = "https://www.gaitameonline.com/rateaj/getrate";
+
+
+export interface Price {
+  high: string
+  open: string
+  bid: string
+  currencyPairCode: string
+  ask: string
+  low: string
+}
+
+export const handler: Handlers<Price | null> = {
+	async GET(_, ctx){
+		const resp = await fetch(url);
+		if (resp.status === 200){
+			const data = await resp.json();
+			const price:Price = data.quotes
+			.filter((obj) => {
+					return obj.currencyPairCode === "USDJPY"
+				});
+			return ctx.render(price[0]);
+		}
+		return ctx.render(null);
+	}
+}
+
+export default function Home({data} : PageProps<Price|null>) {
+	if (!data){
+		return <h1>Data is not available</h1>
+	}
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
+    <div class={tw`w-screen h-screen bg-gray-900`}>
+    <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <img
-        src="/logo.svg"
-        class="w-32 h-32"
+        src="/dollar_yen.svg"
+				width="200px"
+				class="mx-auto"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
-      <p class="my-6">
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
+      <p class="my-10 text(center 3xl white)">
+			  ドル円為替チェッカー
       </p>
-      <Counter start={3} />
+			<p class={tw`my-10 text(center 2xl white)`}>
+			  日本円からドル: {data.open}円
+			</p>
     </div>
+		</div>
   );
 }
